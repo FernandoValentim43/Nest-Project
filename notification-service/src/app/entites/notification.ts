804c4cabe5093b229b1521df12bc510a461d8a1f
FernandoValentim43 <fernandoreal.valentim@gmail.com>
2022-12-15 18:23:07 -0300

    class build up

--- notification-service/src/app/entites/notification.ts
+++ notification-service/src/app/entites/notification.ts
@@ -1,3 +1,21 @@
-class notification {
-    
-}
+export interface NotificationProps {
+    content: string ;
+    category: string ;
+};
+
+export class Notification {
+    private props: NotificationProps;
+
+    constructor() {
+         this.props.content = ""
+    }
+
+    public set content(content: string) {
+        this.props.content = content;
+    }
+
+    public get content() {
+        return this.props.content;
+    }
+};
+
