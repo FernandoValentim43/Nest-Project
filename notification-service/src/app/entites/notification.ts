188934f43d214df8815155f7cf6af8dd9a2086dd
FernandoValentim43 <fernandoreal.valentim@gmail.com>
2022-12-15 18:33:42 -0300

    class build up

--- notification-service/src/app/entites/notification.ts
+++ notification-service/src/app/entites/notification.ts
@@ -1,21 +1,45 @@
 export interface NotificationProps {
-    content: string ;
-    category: string ;
-};
+  recipientId: string;
+  content: string;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+}
 
 export class Notification {
-    private props: NotificationProps;
+  private props: NotificationProps;
 
-    constructor() {
-         this.props.content = ""
-    }
+  constructor(props: NotificationProps) {
+    this.props = props;
+  }
 
-    public set content(content: string) {
-        this.props.content = content;
-    }
+  //getters and setters
 
-    public get content() {
-        return this.props.content;
-    }
-};
+  public set content(content: string) {
+    this.props.content = content;
+  }
 
+  public get content() {
+    return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category() {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+}
