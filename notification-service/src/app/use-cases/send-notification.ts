c9584c7bd60af700e00b3d21011882d975f2c3c5
FernandoValentim43 <fernandoreal.valentim@gmail.com>
2022-12-16 19:12:09 -0300

    .

--- notification-service/src/app/use-cases/send-notification.ts
+++ notification-service/src/app/use-cases/send-notification.ts
@@ -1,5 +1,6 @@
 import { Notification } from "../entites/notification";
 import { notificationContent } from "../entites/notification-content";
+import { NotificationRepository } from "../repositories/notifications-repositorie";
 
 interface SendNotificationRequest {
     recipientId: string;
@@ -12,6 +13,11 @@ interface SendNotificationResponse {
 }
 
 export class SendNotification {
+    constructor(private notificationsRepository: NotificationRepository) {
+
+    }
+
+
     async execute(request: SendNotificationRequest): Promise<SendNotificationResponse> {
         const {recipientId, content, category } = request
         const notification = new Notification({
@@ -19,6 +25,8 @@ export class SendNotification {
             content: new notificationContent(content),
             category,
         })
+
+        await this.notificationsRepository.create(notification)
 
         return {
             notification,
